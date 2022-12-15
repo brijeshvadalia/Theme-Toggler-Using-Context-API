a35e2009c55362bae3f94d52b873042717be0cee
B-8128 <brijeshvadaliya1@gmail.com>
2022-12-15 19:14:22 +0530

    Adding colors

---
+++ src/Colors.js
@@ -0,0 +1,13 @@
+const AppTheme = {
+    light: {
+      textColor: "#000",
+      backgroundColor: "#FFF",
+    },
+    dark: {
+      textColor: "#fff",
+      backgroundColor: "#333",
+    }
+  };
+  
+  export default AppTheme;
+  
